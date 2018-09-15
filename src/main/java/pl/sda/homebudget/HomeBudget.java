0f50f3c3a835551fac6b665130fe0189f6355efa
Liwoszka <liwosz@gmail.com>
2018-09-15 15:09:50 +0200

    [add] home page

---
+++ src/main/java/pl/sda/homebudget/HomeBudget.java
@@ -0,0 +1,11 @@
+package pl.sda.homebudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBudget {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeBudget.class, args);
+    }
+}
